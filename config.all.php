224c804bde2c6b1a3f0214ae77b74d997d34a2c9
root <roberto.stzutski.com>
2022-02-19 19:38:47 -0300

    functions.all.php

--- config.all.php
+++ config.all.php
@@ -1,3 +1,9 @@
-<?php 
+<?php
+session_start();
+
+define('DBHOST','localhost');
+define('DBNAME','blingsinc');
+define('DBUSER','root');
+define('DBPWD','32125');
 
 $apikey_bling = "80abacee149bf99db5e7da4a8f371895429c1aaeb58934c8b8e5351ac4bc95bc527d5db5";
