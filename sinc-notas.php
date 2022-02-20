6642aa3f963215c597c0fd3626cc11930cc7ee4d
root <roberto.stzutski.com>
2022-02-20 20:33:53 -0300

    removido teste da master e renomeado sandbox.php para sinc-nota.php

--- sinc-notas.php
+++ sinc-notas.php
@@ -1,14 +1,13 @@
 <?php
 include_once('config.all.php');
 ?>
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-	<title>REPORTS GEN</title>
-	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
-	<meta name="generator" content="Geany 1.36" />
+<title>SINC NOTAS NFE</title>
+<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+<meta name="generator" content="Geany 1.36" />
 </head>
 <body onload="javascript:window.scrollTo(0,document.body.scrollHeight);">
 
